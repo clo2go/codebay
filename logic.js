ae571759fc37fb6c63df708433b9713cd1a4cb3f
clo2go <clo2go@gmail.com>
2017-01-26 11:57:54 -0600

    initial html, and firebase

---
+++ logic.js
@@ -0,0 +1,9 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDwHEvUBVTxsKuxxh-EESf4PtIbvVK2yrE",
+    authDomain: "codebay-d4bfa.firebaseapp.com",
+    databaseURL: "https://codebay-d4bfa.firebaseio.com",
+    storageBucket: "codebay-d4bfa.appspot.com",
+    messagingSenderId: "1082414080103"
+  };
+  firebase.initializeApp(config);
